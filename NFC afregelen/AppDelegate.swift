02498beec51fc37730b8ef3873e526cde32ae13e
Romuald <romke@me.com>
2015-06-10 21:58:18 +0200

    Initial Commit

---
+++ NFC afregelen/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NFC afregelen
+//
+//  Created by Romuald Dufaux on 10/06/15.
+//  Copyright (c) 2015 Romuald Dufaux. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ NFC afregelen/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NFC afregelen
+//
+//  Created by Romuald Dufaux on 10/06/15.
+//  Copyright (c) 2015 Romuald Dufaux. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
